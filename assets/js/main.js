c91e5fcce7f758884c06a6ae1d9b23e9387d7950
ishwor <prataya2@gmail.com>
2024-05-03 14:00:56 +0545

    optimizing site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,6 @@
 
-// $(".drawer").drawer();
-
-$(".drawer-toggle").click(function () {
-  var targetDrawer = $("#" + $(this).data("target"));
-
-  // Close all open drawers except the one being opened
-  $(".drawer").not(targetDrawer).removeClass("drawer-open");
-
-  // Toggle the open class for the clicked drawer
-  targetDrawer.toggleClass("drawer-open");
-});
-
 // nice select
 $("select").niceSelect();
-
-// navbar
-$(".navigation-portrait").hide();
-setTimeout(function () {
-  $(".navigation-portrait").show();
-}, 100); // Change the delay time as needed
-
 
 // Homepage main slider
 $(".mainslider").slick({
@@ -315,24 +296,6 @@ $(".mainTopic").slick({
 });
 
 
-// popup
-
-const $openModalButton = $("#openModal");
-const $closeModalButton = $("#closeModal");
-const $modal = $("#modal");
-
-$openModalButton.click(function () {
-  $modal.removeClass("hidden");
-});
-
-$closeModalButton.click(function () {
-  $modal.addClass("hidden");
-});
-
-$modal.find(".modal-overlay").click(function () {
-  $modal.addClass("hidden");
-});
-
 // expansion
 
 $(".expansionlist > a").on("click", function (e) {
@@ -353,60 +316,6 @@ $(".expansionlist > a").on("click", function (e) {
     $(this).siblings(".expansioncontent").slideDown(200);
   }
   e.preventDefault();
-});
-
-
-// filter
-
-$(".accordion").on("click", function () {
-  $(this).toggleClass("active");
-  $(this).next().slideToggle(200);
-  if ($(".accordion.active").length > 0) {
-    $(".toggleText").text("Show Filters");
-  } else {
-    $(".toggleText").text("Hide Filters");
-  }
-});
-
-if (window.innerWidth > 768) {
-
-  var toggleAllButton = $("#togglefilter");
-  if ($(".accordion.active").length > 0) {
-    $(".toggleText").text("Show Filters");
-  } else {
-    $(".toggleText").text("Hide Filters");
-  }
-
-  $("#togglefilter, .drawer-toggle").on("click", function () {
-    var allAccordions = $(".accordion");
-    var allContents = $(".filtercontent");
-    var isAllCollapsed = allAccordions.filter(".active").length === 0;
-
-    if (isAllCollapsed) {
-      allAccordions.addClass("active");
-      allContents.slideUp(200);
-    } else {
-      allAccordions.removeClass("active");
-      allContents.slideDown(200);
-    }
-
-    // Update button text based on state
-    $(".toggleText").text(isAllCollapsed ? "Show Filters" : "Hide Filters");
-  });
-}
-
-// view more
-
-$(".show-more-btn").click(function () {
-  const hiddenContent = $(this).prev(".filter-hidden");
-  hiddenContent.slideToggle();
-
-  $(this).toggleClass("active");
-  if ($(this).hasClass("active")) {
-    $(this).find(".viewmore_text").text("Show Less");
-  } else {
-    $(this).find(".viewmore_text").text("Show More");
-  }
 });
 
 // Video play
