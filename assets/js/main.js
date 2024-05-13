d4d0bc90b5f929c2e2247338986bacec0b99650f
ishwor <prataya2@gmail.com>
2024-05-13 16:36:41 +0545

    design update

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,14 +53,14 @@ $("#serviceSlider").slick({
     {
       breakpoint: 1280,
       settings: {
-        slidesToShow: 4,
+        slidesToShow: 3,
         slidesToScroll: 1,
       },
     },
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
+        slidesToShow: 2,
         slidesToScroll: 1,
       },
     },
